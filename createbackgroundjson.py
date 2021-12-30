d70124a5c0f5882159cf61698921fe9b0c342f6e
NicholasBlackburn1 <nickblackburn02@gmail.com>
2021-12-30 12:25:12 -0500

    got pyhtong to remove old json

--- createbackgroundjson.py
+++ createbackgroundjson.py
@@ -25,22 +25,25 @@ def readConfig(i):
 
 # creates basic json structurie
 def create_baseFile(id,basedir,fileextention):
+    if(os.path.exists('src/main/resources/assets/minecraft/blackburn/backgrounds.json')):
+        os.remove('src/main/resources/assets/minecraft/blackburn/backgrounds.json')
+    else:
 
-    print("Starting to Open file for writing")
-    while id < 26:
-        data = {'id':id,'image':str(basedir)+str(id)+str(fileextention),'buttonLenght':addToFile(id,"buttonLength"),'sp_posX': addToFile(id,"sp_posX"), 'sp_posY':addToFile(id,"sp_posY"),'mp_posX':addToFile(id,"mp_posX"),'mp_posY':addToFile(id,"mp_posY"),'settings_posX':addToFile(id,"settings_posX"),
-        'settings_posY':addToFile(id,"settings_posY"), 'quit_posX':addToFile(id,"quit_posX"), 'quit_posY':addToFile(id,"quit_posY"),'splash_posX':addToFile(id,"splash_posX"), 
-        'splash_posY':addToFile(id,"splash_posY"),'edition_posX':addToFile(id,"edition_posX"),'edition_posY':addToFile(id,"edition_posY"),'edition_posXNonFull':addToFile(id,"edition_posX_nonfull"),'edition_posy_nonfull':addToFile(id,"edition_posy_nonfull"),
-        'lang_posX':addToFile(id,"lang_posX"), 'lang_posY':addToFile(id,"lang_posY")}
-     
-        output.append(data)
-        id +=1
-
-        if(id == 26):
-            with open('src/main/resources/assets/minecraft/blackburn/backgrounds.json', 'a', encoding='utf8') as outfile:
-                print(json.dumps(data, sort_keys=False, indent=4))
-                json.dump(output, outfile, sort_keys=False, indent=4)
+        print("Starting to Open file for writing")
+        while id < 26:
+            data = {'id':id,'image':str(basedir)+str(id)+str(fileextention),'buttonLenght':addToFile(id,"buttonLength"),'sp_posX': addToFile(id,"sp_posX"), 'sp_posY':addToFile(id,"sp_posY"),'mp_posX':addToFile(id,"mp_posX"),'mp_posY':addToFile(id,"mp_posY"),'settings_posX':addToFile(id,"settings_posX"),
+            'settings_posY':addToFile(id,"settings_posY"), 'quit_posX':addToFile(id,"quit_posX"), 'quit_posY':addToFile(id,"quit_posY"),'splash_posX':addToFile(id,"splash_posX"), 
+            'splash_posY':addToFile(id,"splash_posY"),'edition_posX':addToFile(id,"edition_posX"),'edition_posY':addToFile(id,"edition_posY"),'edition_posXNonFull':addToFile(id,"edition_posX_nonfull"),'edition_posy_nonfull':addToFile(id,"edition_posy_nonfull"),
+            'lang_posX':addToFile(id,"lang_posX"), 'lang_posY':addToFile(id,"lang_posY")}
         
+            output.append(data)
+            id +=1
+
+            if(id == 26):
+                with open('src/main/resources/assets/minecraft/blackburn/backgrounds.json', 'a', encoding='utf8') as outfile:
+                    print(json.dumps(data, sort_keys=False, indent=4))
+                    json.dump(output, outfile, sort_keys=False, indent=4)
+            
 # adds keys that are missing into file 
 def addToFile(id,key):
    
