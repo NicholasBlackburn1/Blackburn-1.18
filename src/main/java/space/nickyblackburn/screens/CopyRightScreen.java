1913ff7a5d52c708afe9336af60bd8cf6dbe1c7b
NicholasBlackburn1 <nickblackburn02@gmail.com>
2022-01-05 13:44:08 -0500

    got basic copyright page working

--- src/main/java/space/nickyblackburn/screens/CopyRightScreen.java
+++ src/main/java/space/nickyblackburn/screens/CopyRightScreen.java
@@ -1,4 +1,8 @@
 package space.nickyblackburn.screens;
+import java.io.File;
+import java.net.URISyntaxException;
+import java.util.Date;
+
 /***
  * this class is for displaying my custom Version screen and info about the client 
  */
@@ -7,6 +11,7 @@ import com.mojang.blaze3d.vertex.PoseStack;
 import net.minecraft.client.gui.screens.Screen;
 import net.minecraft.network.chat.Component;
 import net.minecraft.network.chat.TextComponent;
+import space.nickyblackburn.utils.Consts;
 
 public class CopyRightScreen extends Screen{
 
@@ -20,6 +25,19 @@ public class CopyRightScreen extends Screen{
         this.renderBackground(p_96295_);
         
         drawCenteredString(p_96295_, this.font, this.title, this.width / 2, this.height / 4 - 60 + 20, 16777215);
-        drawString(p_96295_, this.font, "OwO Daddy.", this.width / 2 - 140, this.height / 4 - 60 + 60 + 81, 10526880);
+        
+        this.drawCenteredString(p_96295_, this.font,"§6Blackburn Software Stats UwU",this.width/2 ,140, 16777215);
+        this.drawCenteredString(p_96295_, this.font,"§6Version:"+"§r "+Consts.VERSION+Consts.RELEASE,this.width/2 ,150, 16777215);
+        this.drawCenteredString(p_96295_, this.font,"§6Build:"+"§r "+Consts.RELEASE,this.width/2 ,160, 16777215);
+        try {
+			File jarFile = new File
+			(this.getClass().getProtectionDomain().getCodeSource().getLocation().toURI());
+
+            this.drawCenteredString(p_96295_, this.font,"§6BuildDate:"+"§r "+new Date(jarFile.lastModified()),this.width/2 ,170, 16777215);
+            
+		} catch (URISyntaxException e1) {
+            Consts.error(e1.getMessage());
+		}
+      
     }
 }
