4ef3f5d2a3ef618ea274b154fc61715871b3f7b2
NicholasBlackburn1 <nickblackburn02@gmail.com>
2022-02-10 12:40:41 -0500

    working on twitch gui

--- src/main/java/space/nickyblackburn/screens/TwitchOptionsScreen.java
+++ src/main/java/space/nickyblackburn/screens/TwitchOptionsScreen.java
@@ -10,13 +10,17 @@ import java.util.List;
 import com.mojang.blaze3d.vertex.PoseStack;
 
 import net.minecraft.client.Options;
+import net.minecraft.client.gui.components.EditBox;
 import net.minecraft.client.gui.screens.Screen;
+import net.minecraft.client.resources.language.I18n;
+import net.minecraft.commands.arguments.ColorArgument;
 import net.minecraft.network.chat.Component;
 import net.minecraft.network.chat.TextComponent;
 import net.minecraft.network.chat.TranslatableComponent;
 import net.optifine.Lang;
 import net.optifine.gui.GuiScreenButtonOF;
 import net.optifine.gui.GuiScreenOF;
+import net.optifine.util.GuiRect;
 
 public class TwitchOptionsScreen extends GuiScreenOF{
 
@@ -48,22 +52,29 @@ public class TwitchOptionsScreen extends GuiScreenOF{
         drawCenteredString(pPoseStack, this.minecraft.font, this.title, this.width / 2, 15, 16777215);
         drawString(pPoseStack, this.minecraft.font, s2, this.width - i - 2, this.height - 10, 8421504);
 
-        buttonLayout();
+        buttonLayout(pPoseStack);
 
         super.render(pPoseStack, pMouseX, pMouseY, pPartialTick);
       
     }
 
     // allows me to set up custom buttons in the menu
-    private void buttonLayout(){
+    private void buttonLayout(PoseStack pPoseStack){
         int l = this.height / 6 + 21 * 5 / 2 - 12;
-    
-        this.addRenderableWidget(new GuiScreenButtonOF(302, 0, l+ 05, Lang.get("blackburn.twitch.email")));
-        this.addRenderableWidget(new GuiScreenButtonOF(303, 0, l+ 30, Lang.get("blackburn.twitch.password")));
+        
+        // this is the entry box for the componet email 
+        this.drawString(pPoseStack, this.minecraft.font, I18n.a("blackburn.twitch.email"),0, l+ 10, 16777215);
+        this.addRenderableWidget(new EditBox(this.font, 0, l+ 05 , 200, 20, new TranslatableComponent("blackburn.twitch.email")));
+
+        // entry box for password
+        this.drawString(pPoseStack, this.minecraft.font, I18n.a("blackburn.twitch.password"),0, l+ 25, 16777215);
+        this.addRenderableWidget(new EditBox(this.font, 0, l+ 30, 200, 20, new TranslatableComponent("blackburn.twitch.password")));
+
+        // cbuttons
         this.addRenderableWidget(new GuiScreenButtonOF(304, 0, l+ 60, Lang.get("blackburn.twitch.connect")));
         this.addRenderableWidget(new GuiScreenButtonOF(305, 0, l+90, Lang.get("blackburn.twitch.exit")));
-      
-    }
+       
+    }   
 
 
 
